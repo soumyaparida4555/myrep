8a6b950b9b76909df4e585bfa6ca489113f7eacb
Soumya <soumyaparida4555@gmail.com>
2025-08-03 19:51:58 +0530

    changed

--- src/component/About.js
+++ src/component/About.js
@@ -6,7 +6,7 @@ const About=()=>{
 
         <div>
       
-      <h2>we are providing the best recomended higine food,with fast delivery food</h2>
+      <h2>we are providing the best recomended higine food,with fast delivery food ,we are not applying any platform fees </h2>
 
 
 
